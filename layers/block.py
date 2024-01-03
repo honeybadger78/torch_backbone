43c68331d7996bc3db989f89c74fccb99efe7a12
honeybadger78 <allyouneed3002@gmail.com>
2024-01-03 09:24:29 +0000

    Refactor code to use Conv class from layers.conv module

--- layers/block.py
+++ layers/block.py
@@ -1,4 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from .conv import Conv, DepthWiseConv
+from layers.conv import Conv
+
+x = Conv(in_channels=3, out_channels=32, kernel_size=3, stride=1)
+print(x)
